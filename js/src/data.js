b12aa725c180494a4963ec577247dfaa03422757
Reilly Herrewig-Pope <reilly@herrewig.net>
2024-12-27 20:55:40 -0500

    use correct URL

--- js/src/data.js
+++ js/src/data.js
@@ -1,7 +1,7 @@
 export function fetchQuiz() {
     const url = window.location.hostname === 'localhost'
-    ? 'http://localhost:8081/api/v1/quiz' // Localdev
-    : 'https://api.tomedome.io';          // Production API URL
+    ? 'http://localhost:8081/api/v1/quiz'       // Localdev
+    : 'https://api.tomedome.io/api/v1/quiz';    // Production API URL
     return fetch(url).then(response => response.json());
 }
 
