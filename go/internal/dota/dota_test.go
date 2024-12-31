b2bc499287d0181d2635fa184510d3aa476a44d4
Reilly Herrewig-Pope <reilly@herrewig.net>
2024-12-30 21:00:38 -0500

    Remove impossible-to-upkeep test

--- go/internal/dota/dota_test.go
+++ go/internal/dota/dota_test.go
@@ -2,8 +2,6 @@ package dota
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestPrimaryAttributeString(t *testing.T) {
@@ -23,37 +21,4 @@ func TestPrimaryAttributeString(t *testing.T) {
 			t.Errorf("got %q, want %q", got, tt.want)
 		}
 	}
-}
-
-func TestHeroGetQuizJson(t *testing.T) {
-	hero := Hero{
-		Id:               4,
-		ShortName:        "bloodseeker",
-		DisplayName:      "Bloodseeker",
-		Description:      "Bloodseeker forces difficult decisions on his enemies...",
-		Lore:             "Strygwyr the Bloodseeker...",
-		AttackType:       "Melee",
-		PrimaryAttribute: "agi",
-		Abilities: []Ability{
-			{
-				Id:          5015,
-				Button:      "Q",
-				ShortName:   "bloodseeker_bloodrage",
-				DisplayName: "Bloodrage",
-				Description: "Drives bloodseeker...",
-			},
-			{
-				Id:          5017,
-				Button:      "Passive",
-				ShortName:   "bloodseeker_thirst",
-				DisplayName: "Thirst",
-				Description: "Bloodseeker is invigorated...",
-			},
-		},
-	}
-
-	got := string(hero.GetQuizJson())
-	want := `{"attackType":"Melee","description":"Bloodseeker forces difficult decisions on his enemies...","displayName":"Bloodseeker","primaryAttribute":"Agility","questions":[{"abilityName":"Bloodrage","answer":"Drives bloodseeker...","prompt":"Which ability is mapped to [Q]?"},{"abilityName":"Thirst","answer":"Bloodseeker is invigorated...","prompt":"Describe passive ability"}],"shortName":"bloodseeker"}`
-
-	assert.JSONEq(t, want, got)
 }
